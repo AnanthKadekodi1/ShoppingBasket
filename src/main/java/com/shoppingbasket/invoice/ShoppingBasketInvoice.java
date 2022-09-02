9da4bdd4ec18cf53852d05bd891fc0fdb68286ce
Ananth Kadekodi <kadekodiananth@gmail.com>
2022-09-02 22:46:44 +0800

    fixed to pounds

--- src/main/java/com/shoppingbasket/invoice/ShoppingBasketInvoice.java
+++ src/main/java/com/shoppingbasket/invoice/ShoppingBasketInvoice.java
@@ -7,20 +7,20 @@ import java.util.Locale;
 @Getter
 public class ShoppingBasketInvoice {
 
-    private static final String invoiceCurrencySymbol = "$";
-    private int dollar;
-    private int cents;
-    private int totalInCents;
+    private static final String invoiceCurrencySymbol = "£";
+    private int pound;
+    private int pence;
+    private int totalPence;
 
     public ShoppingBasketInvoice(int total) {
         if (total < 0)
             throw new IllegalArgumentException("Total cost cannot be negative");
-        totalInCents = total;
-        dollar = total / 100;
-        cents = total % 100;
+        totalPence = total;
+        pound = total / 100;
+        pence = total % 100;
     }
 
     public String toString() {
-        return String.format("Total cost of shopping basket: %s%,d.%02d", invoiceCurrencySymbol, dollar, cents);
+        return String.format("Total cost of shopping basket: %s%,d.%02d", invoiceCurrencySymbol, pound, pence);
     }
 }
--- src/test/java/com/shoppingbasket/ShoppingBasketInvoiceTest.java
+++ src/test/java/com/shoppingbasket/ShoppingBasketInvoiceTest.java
@@ -9,36 +9,36 @@ public class ShoppingBasketInvoiceTest {
     @Test
     public void returnZeroWhenTotalIsZero() {
         ShoppingBasketInvoice shoppingBasketInvoice = new ShoppingBasketInvoice(0);
-        Assert.assertEquals(0, shoppingBasketInvoice.getTotalInCents());
-        Assert.assertEquals(0, shoppingBasketInvoice.getCents());
-        Assert.assertEquals(0, shoppingBasketInvoice.getDollar());
+        Assert.assertEquals(0, shoppingBasketInvoice.getTotalPence());
+        Assert.assertEquals(0, shoppingBasketInvoice.getPence());
+        Assert.assertEquals(0, shoppingBasketInvoice.getPound());
     }
 
     @Test
     public void correctDisplayWithFiftyCents() {
         ShoppingBasketInvoice shoppingBasketInvoice = new ShoppingBasketInvoice(50);
-        Assert.assertEquals(50, shoppingBasketInvoice.getTotalInCents());
-        Assert.assertEquals(50, shoppingBasketInvoice.getCents());
-        Assert.assertEquals(0, shoppingBasketInvoice.getDollar());
-        Assert.assertEquals("Total cost of shopping basket: $0.50", shoppingBasketInvoice.toString());
+        Assert.assertEquals(50, shoppingBasketInvoice.getTotalPence());
+        Assert.assertEquals(50, shoppingBasketInvoice.getPence());
+        Assert.assertEquals(0, shoppingBasketInvoice.getPound());
+        Assert.assertEquals("Total cost of shopping basket: £0.50", shoppingBasketInvoice.toString());
     }
 
     @Test
     public void correctDisplayWithFiveDollars() {
         ShoppingBasketInvoice shoppingBasketInvoice = new ShoppingBasketInvoice(500);
-        Assert.assertEquals(500, shoppingBasketInvoice.getTotalInCents());
-        Assert.assertEquals(0, shoppingBasketInvoice.getCents());
-        Assert.assertEquals(5, shoppingBasketInvoice.getDollar());
-        Assert.assertEquals("Total cost of shopping basket: $5.00", shoppingBasketInvoice.toString());
+        Assert.assertEquals(500, shoppingBasketInvoice.getTotalPence());
+        Assert.assertEquals(0, shoppingBasketInvoice.getPence());
+        Assert.assertEquals(5, shoppingBasketInvoice.getPound());
+        Assert.assertEquals("Total cost of shopping basket: £5.00", shoppingBasketInvoice.toString());
     }
 
     @Test
     public void correctDisplayWithFiveDollarsFiftyCents() {
         ShoppingBasketInvoice shoppingBasketInvoice = new ShoppingBasketInvoice(550);
-        Assert.assertEquals(550, shoppingBasketInvoice.getTotalInCents());
-        Assert.assertEquals(50, shoppingBasketInvoice.getCents());
-        Assert.assertEquals(5, shoppingBasketInvoice.getDollar());
-        Assert.assertEquals("Total cost of shopping basket: $5.50", shoppingBasketInvoice.toString());
+        Assert.assertEquals(550, shoppingBasketInvoice.getTotalPence());
+        Assert.assertEquals(50, shoppingBasketInvoice.getPence());
+        Assert.assertEquals(5, shoppingBasketInvoice.getPound());
+        Assert.assertEquals("Total cost of shopping basket: £5.50", shoppingBasketInvoice.toString());
     }
 
     @Test(expected = IllegalArgumentException.class)
